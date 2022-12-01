406b57333f27cf250026b0a7469b25c5923613ad
derekstride <derek.stride@shopify.com>
2022-12-01 15:50:01 -0500

    Refactor to useful shared primitive

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,35 +1,19 @@
 use advent_of_code_2022::*;
-
 use anyhow::Result;
 
-const INPUTS: &'static str = include_str!("../../inputs/day01.txt");
+const INPUT: &'static str = include_str!("../../inputs/day01.txt");
 
-fn elf_inventory() -> Result<Vec<usize>> {
-    let inv = INPUTS
-        .lines()
-        .fold(vec![0], |mut acc, line| {
-            if line == "" {
-                acc.push(0);
-            } else {
-                *acc.last_mut().unwrap() += line.parse::<usize>().unwrap();
-            };
-            acc
-        });
-    Ok(inv)
-}
+fn main() -> Result<()> {
+    let mut elf_inventory = split_input(INPUT.trim(), "\n\n", |s| {
+        let r = split_input(s, "\n", |s| Ok(s.parse::<usize>()?))?;
+        Ok(r.iter().sum::<usize>())
+    })?;
 
-fn main() {
-    let inv = elf_inventory().unwrap();
-    let mut sorted_inv = inv
-        .iter()
-        .enumerate()
-        .map(|(i, total)| (i + 1, *total))
-        .collect::<Vec<(usize, usize)>>();
-    sorted_inv.sort_by(|(_, a), (_, b)| b.cmp(a));
+    elf_inventory.sort_by(|a, b| b.cmp(a));
+    println!("Part 1: {:?}", elf_inventory[0]);
 
-    let total_snacks: usize = sorted_inv[..3]
-        .iter()
-        .map(|(_, total)| total)
-        .sum();
-    println!("Day 1: {:?}", total_snacks);
+    let total_snacks: usize = elf_inventory[..3].iter().sum();
+    println!("Part 2: {:?}", total_snacks);
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,90 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use anyhow::Result;
+
+pub fn split_input<F, T>(input: &str, delim: &str, func: F) -> Result<Vec<T>>
+    where F: Fn(&str) -> Result<T> {
+        input
+            .split(delim)
+            .map(|s| func(s))
+            .collect::<Result<Vec<T>>>()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn make_input(input: &str) -> String {
+        input
+            .lines()
+            .skip(1)
+            .map(|s| s.trim())
+            .collect::<Vec<&str>>()
+            .join("\n")
+            .to_string()
+    }
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn split_input_chunks_on_delimiter() -> Result<()> {
+        let input = make_input(
+            r###"
+            1000
+            2000
+            3000
+
+            4000
+
+            5000
+            6000
+
+            7000
+            8000
+            9000
+
+            10000
+            "###
+        );
+
+        let expected = vec![
+            "1000\n2000\n3000",
+            "4000",
+            "5000\n6000",
+            "7000\n8000\n9000",
+            "10000\n",
+        ];
+        let result = split_input(&input, "\n\n", |s| Ok(s.to_string()))?;
+
+        assert_eq!(expected, result);
+
+        Ok(())
+    }
+
+    #[test]
+    fn split_input_with_inner_split() -> Result<()> {
+        let input = make_input(
+            r###"
+            1000
+            2000
+            3000
+
+            4000
+
+            5000
+            6000
+
+            7000
+            8000
+            9000
+
+            10000
+            "###
+        );
+        let expected = vec![6000, 4000, 11000, 24000, 10000];
+        let result = split_input(&input, "\n\n", |s| {
+            let r = split_input(s.trim(), "\n", |s| Ok(s.parse::<usize>()?))?;
+            Ok(r.iter().sum::<usize>())
+        })?;
+
+        assert_eq!(expected, result);
+
+        Ok(())
     }
 }
